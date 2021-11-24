fcf73d4b67fed093203779fd45f5a4fc4e15cabe
Damilare Darmie Akinlaja <dreakinlaja@gmail.com>
2021-11-24 11:45:58 +0100

    use pg-query-emscripten

--- packages/parser/src/index.js
+++ packages/parser/src/index.js
@@ -1,16 +1,14 @@
 import { Deparser, deparse } from 'pgsql-deparser';
 import { enums } from 'pgsql-enums';
 import { parseQuerySync, parsePlPgSQLSync as parseFunction } from 'libpg-query';
+import PgQuery from 'pg-query-emscripten';
 
 export const parse = (sql) => {
   if (!sql) throw new Error('no SQL provided to parser');
-  const result = parseQuerySync(sql);
-  return result.stmts.map(({ stmt, stmt_len }) => {
+  const result = PgQuery.parse(sql); //parseQuerySync(sql);
+  return result.parse_tree.map((s)=>{
     return {
-      RawStmt: {
-        stmt,
-        stmt_len
-      }
+      ...s
     };
   });
 };
